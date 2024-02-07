9dfdf4dd012ab7facc50fd7676360895c48e5635
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-02-07 21:48:17 +0900

    Swift 기본 문법

--- iOSbasic.playground/Contents.swift
+++ iOSbasic.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 
-//  조건문
+//   ** 조건문 **
 var isDarkMode : Bool = true
 
 if isDarkMode == true {
@@ -16,3 +16,27 @@ var title: String = !isDarkMode == true ? "다크모드 입니다." : "다크모
 print("title: \(title)")
 
  
+// ** for 문 **
+
+// 콜렉션 : 데이터를 모아둔 것
+// 배열, 셋, 딕셔너리, 튜플
+// 배열
+
+var myArray: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for item in myArray where item > 5 {
+    print("item: \(item)")
+}
+// where 를 사용하여 특정한 조건을 넣을 수 있다. item 안에 5보다 큰 수만 넣겠다 라는 뜻
+
+for item in myArray where item % 2 == 0 {
+    print("짝수: \(item)")
+}
+// item을 2로 나눴을때 나머지가 0인 수 (짝수)만 출력
+
+for item in myArray where item % 2 != 0 {
+    print("홀수: \(item)")
+}
+// item을 2로 나눴을때 나머지가 0이 아닌 수(홀수)만 출력
+
+
