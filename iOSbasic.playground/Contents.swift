72db51d3923dca4eee5b2f600cc4d09704c3e46c
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-02-07 22:53:03 +0900

    optional, Unwrapping

--- iOSbasic.playground/Contents.swift
+++ iOSbasic.playground/Contents.swift
@@ -106,3 +106,43 @@ for _ in 0..<25 {
 
 print(randomInts)
 
+// ** optional , unwrapp **
+
+// optional 이란?
+// 값이 있는지 없는지 모른다.
+
+var someVariable: Int? = nil
+
+if someVariable == nil {
+    someVariable = 90
+}
+
+print("someVariable: ", someVariable)
+
+// unwrapp 이란? 랩 즉 감싸져있는 것을 벗기는 것 (옵셔널을 벗기는 것)
+
+if let otherVariable = someVariable {
+    print("언래핑 되었다. 즉 값이 있다. otherVariable: \(otherVariable)")
+} else {
+    print("값이 없다.")
+}
+
+someVariable = nil
+
+// someVariable 이 비어있으면, 즉 값이 없으면 기본값으로 밑에 값을 넣겠다.
+
+let myValue = someVariable ?? 10
+print("myValue: \(myValue)")
+
+
+
+
+func unwrap(_ parameter: Int?){
+    print("unwrap() called")
+    //값이 없으면 리턴 해버린다.
+    //즉 지나간다.
+    guard let unWrappedParam = parameter else {
+        return
+    }
+    print("unWrappedParam: \(unWrappedParam)")
+}
