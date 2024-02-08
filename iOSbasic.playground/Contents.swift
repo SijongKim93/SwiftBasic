acc35b1c3bdeb6bcd9f8777b87ce90e2435df2ca
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-02-08 15:55:45 +0900

    property observer

--- iOSbasic.playground/Contents.swift
+++ iOSbasic.playground/Contents.swift
@@ -146,3 +146,17 @@ func unwrap(_ parameter: Int?){
     }
     print("unWrappedParam: \(unWrappedParam)")
 }
+
+// ** property observer
+
+var myAge = 0 {
+    willSet {
+        print("값이 설정될 예정이다. myAge: \(myAge)")
+    }
+    didSet {
+        print("값이 설정되었다. myAge: \(myAge)")
+    }
+}
+
+myAge = 10
+myAge = 20
